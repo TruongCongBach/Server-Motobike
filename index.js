70820efcf39560f9a2bc4e8639974c75921a9f40
TruongBach <truongcongbach1996@gmail.com>
2019-07-16 22:23:27 +0700

    edit config env

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const Koa = require('koa');
 const app = new Koa();
+const knexFile = require('./database/knexfile');
+const qb = require('knex')(knexFile.development);
 
+
+qb.select().table('table_name').then(res => {
+    console.log(res);
+
+});
 app.use(async ctx => {
     ctx.body = 'Hello World';
 });
