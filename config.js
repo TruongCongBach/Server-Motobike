3c306cab2e0d0f5b2b3b2b157bcbfc37cf660ffd
TruongBach <truongcongbach1996@gmail.com>
2019-07-16 22:22:53 +0700

    add migration

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    database: {
+        host       : process.env.DATABASE_HOST,
+        port       : process.env.DATABASE_PORT,
+        database   : process.env.DATABASE_NAME,
+        user       : process.env.DATABASE_USER ,
+        password   : process.env.DATABASE_PASS
+    }
+};
